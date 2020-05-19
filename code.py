335992320ead76d4343c6e90b12a468e62b8f690
Nitin <55016899+logicaldemon@users.noreply.github.com>
2020-05-19 13:41:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,35 +1,77 @@
 #%%
-import cv2																			#importing opencv
+import cv2
 import numpy as np
 
-# %%
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')        #importing har cass. xml files..
-
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 #%%
-cap = cv2.VideoCapture(0)
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(20, 25)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+gender_list = ['Male', 'Female']
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-while 1:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)						 #convorting rbg to gray scale
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#%%
+def load_caffe_models():
+    age_net = cv2.dnn.readNetFromCaffe('deploy_age.prototxt', 'age_net.caffemodel')
+    gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt', 'gender_net.caffemodel')
+    return(age_net, gender_net)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-    cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff												#press escape to stop camera
-    if k == 27:
-        break
+#%%
+def video_detector(age_net, gender_net):
+    
 
-cap.release()
-cv2.destroyAllWindows()
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 480) 
+    cap.set(4, 640)
+
+    while True:  
+        ret, image = cap.read()
+        counter=0
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+        if(len(faces)>counter):
+            print("Found {} faces".format(str(len(faces))))
+            counter+=1
+        if(len(faces)>counter):
+            counter+=len(faces)
+            
+        for (x, y, w, h )in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+            face_img = image[y:y+h, h:h+w].copy()
+            blob = cv2.dnn.blobFromImage(face_img, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            
+            #gender
+
+            gender_net.setInput(blob)
+            gender_preds = gender_net.forward()
+            gender = gender_list[gender_preds[0].argmax()]
+           # print("Gender : " + gender)
+
+            #age
+
+            age_net.setInput(blob)
+            age_preds = age_net.forward()
+            age = age_list[age_preds[0].argmax()]
+            #print("Age Range: " + age)
+
+        overlay_text = "%s %s" % ('Male', "[20-25]")
+        cv2.putText(image, overlay_text, (x, y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow('frame', image)  
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+#%%
+if __name__ == "__main__":
+    age_net, gender_net = load_caffe_models()
+    video_detector(age_net, gender_net)
+
 
 # %%
